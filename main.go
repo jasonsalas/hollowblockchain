67ddbe5d8eb7890f68c718afed17bdb2f264b218
Jason Salas <jason@kuam.com>
2021-12-03 05:04:11 +1000

    added proof of work algorithm

--- main.go
+++ main.go
@@ -1,65 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/jasonsalas/bc/blockchain"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type Blockchain struct {
-	blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{
-		Hash:     []byte{},
-		Data:     []byte(data),
-		PrevHash: prevHash,
-	}
-	block.DeriveHash()
-	return block
-}
-
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockchain() *Blockchain {
-	return &Blockchain{
-		blocks: []*Block{
-			Genesis(),
-		},
-	}
-}
-
 func main() {
-	chain := InitBlockchain()
+	chain := blockchain.InitBlockchain()
 	chain.AddBlock("Block 001")
 	chain.AddBlock("Block 002")
 	chain.AddBlock("Block 003")
 	chain.AddBlock("Block 004")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("data: %s\n", block.Data)
 		fmt.Printf("previous hash: %x\n", block.PrevHash)
-		fmt.Printf("hash: %x\n\n", block.Hash)
+		fmt.Printf("hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println("----")
 	}
 }
